1537017e067ecaee988d653aac7d784cbe7310f7
Sanaia Pierre <122914227+sapierre26@users.noreply.github.com>
2026-02-20 14:26:55 -0800

    Created a GameCategory type

---
+++ app/src/main/java/dev/csse/dtaylo37/triviago/GameCategory.kt
@@ -0,0 +1,7 @@
+package dev.csse.dtaylo37.triviago
+
+data class GameCategory(
+    val id: Int,
+    val title: String = "",
+    // is it possible to do what is happening with title with icons
+)
--- app/src/main/java/dev/csse/dtaylo37/triviago/ui/ChooseGameCategoryScreen.kt
+++ app/src/main/java/dev/csse/dtaylo37/triviago/ui/ChooseGameCategoryScreen.kt
@@ -1,2 +1,13 @@
 package dev.csse.dtaylo37.triviago.ui
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import dev.csse.dtaylo37.triviago.GameCategory
+
+@Composable
+fun Routes.ChooseGameCategoryScreen(
+    gameCategory: GameCategory,
+    modifier: Modifier = Modifier
+) {
+
+}
