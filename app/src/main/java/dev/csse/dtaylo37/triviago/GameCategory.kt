3d027fa15955eee76f81f8ba7488c58706c36b1a
Sanaia Pierre <122914227+sapierre26@users.noreply.github.com>
2026-03-04 21:00:54 -0800

    Almost done creating the icons needed for each category

--- app/src/main/java/dev/csse/dtaylo37/triviago/GameCategory.kt
+++ app/src/main/java/dev/csse/dtaylo37/triviago/GameCategory.kt
@@ -1,13 +1,19 @@
 package dev.csse.dtaylo37.triviago
 
+import androidx.compose.ui.graphics.Color
+
 enum class GameCategory(
     val displayName: String,
+    val iconRes: Int,
+    val backgroundColor: Color
 ) {
-    HISTORY("History"),
-    GEOGRAPHY("Geography"),
-    SCIENCEMATH("Science & Math"),
-    POPCULTURE("Pop Culture"),
-    SPORTSSGAMES("Sports & Games"),
-    LITERATURE("Literature"),
-    MIXEDKNOWLEDGE("Mixed Knowledge")
+    HISTORY("History", R.drawable.history_icon, Color(0xFFE06659)),
+    GEOGRAPHY("Geography", R.drawable.history_icon, Color(0xFF605371)),
+    SCIENCEMATH("Science & Math", R.drawable.history_icon, Color(0xFF4CB3A0)),
+    POPCULTURE("Pop Culture", R.drawable.history_icon, Color(0xFF2D9DA8)),
+    SPORTSSGAMES("Sports & Games", R.drawable.history_icon, Color(0xFFE7C648)),
+    LITERATURE("Literature", R.drawable.history_icon, Color(0xFF7CBC51)),
+    MIXEDKNOWLEDGE("Mixed Knowledge", R.drawable.history_icon, Color(0xFF5B4D4D))
 }
+
+//Museum by mieestore from <a href="https://thenounproject.com/browse/icons/term/museum/" target="_blank" title="Museum Icons">Noun Project</a> (CC BY 3.0)
--- app/src/main/java/dev/csse/dtaylo37/triviago/ui/ChooseGameCategoryScreen.kt
+++ app/src/main/java/dev/csse/dtaylo37/triviago/ui/ChooseGameCategoryScreen.kt
@@ -1,14 +1,21 @@
 package dev.csse.dtaylo37.triviago.ui
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.captionBarPadding
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+//import androidx.compose.material3.R
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -17,8 +24,13 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import dev.csse.dtaylo37.triviago.GameCategory
@@ -57,18 +69,45 @@ fun ChooseGameCategoryScreen() {
                 Text("To Start, Choose a Category: ")
 
                 Spacer(modifier = Modifier.height(16.dp))
+
+                LazyVerticalGrid(
+                    columns = GridCells.Fixed(2),
+                    Modifier.weight(1f),
+                    verticalArrangement = Arrangement.spacedBy(16.dp),
+                    horizontalArrangement = Arrangement.spacedBy(16.dp)
+                ) {
+                    items(GameCategory.entries) { category ->
+                        GameCategoryCard(
+                            value = category
+                        )
+                    }
+                }
             }
         }
         TriviaGoAppBar(title = screenTitleText)
     }
 }
 
-//@Composable
-//fun GameCategoryCard(
-//    value: GameCategory,
-//    modifier: Modifier = Modifier,
-//) {
-//    Box(
-//
-//    )
-//}
+@Composable
+fun GameCategoryCard(
+    value: GameCategory,
+    modifier: Modifier = Modifier,
+) {
+    Box(
+        modifier = modifier
+            .aspectRatio(0.75f)
+            .background(
+                color = value.backgroundColor,
+                shape = RoundedCornerShape(20.dp)
+            )
+            .padding(16.dp),
+        contentAlignment = Alignment.Center
+    ) {
+        Image(
+            painter = painterResource(id = value.iconRes),
+            contentDescription = value.displayName,
+            modifier = Modifier.fillMaxSize(0.6f),
+            contentScale = ContentScale.Fit
+        )
+    }
+}
