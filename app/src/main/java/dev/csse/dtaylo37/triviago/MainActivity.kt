a6690695b88bca50776e6a609d376c757ea03031
Sanaia Pierre <122914227+sapierre26@users.noreply.github.com>
2026-03-06 03:28:27 -0800

    Began setup for the routes from each category

--- app/src/main/java/dev/csse/dtaylo37/triviago/MainActivity.kt
+++ app/src/main/java/dev/csse/dtaylo37/triviago/MainActivity.kt
@@ -7,10 +7,13 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import dev.csse.dtaylo37.triviago.ui.*
 import dev.csse.dtaylo37.triviago.ui.theme.TriviaGOTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +22,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             TriviaGOTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                TriviaGoApp()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TriviaGOTheme {
-        Greeting("Android")
     }
 }
--- app/src/main/java/dev/csse/dtaylo37/triviago/ui/TriviaGoApp.kt
+++ app/src/main/java/dev/csse/dtaylo37/triviago/ui/TriviaGoApp.kt
@@ -1,29 +1,65 @@
 package dev.csse.dtaylo37.triviago.ui
 
-import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import kotlinx.serialization.Serializable
 
 sealed class Routes {
     @Serializable
-    data object ChooseGameCategory // home screen
+    data object Home
+
+    @Serializable
+    data class Loading(val category: String, val seconds: Int)
+
+    @Serializable
+    data object ProfileSettings
+
+    @Serializable
+    data object MultipleChoiceQuestion
+
+    @Serializable
+    data object FillInTheBlankQuestion
+
+    @Serializable
+    data object RearrangeTheOrderQuestion
+
+    @Serializable
+    data object TrueOrFalseQuestion
+
+    @Serializable
+    data object MatchingQuestion
 }
 
-//@Composable
-//fun TriviaGoApp() {
-//    NavHost() {
-//        composable<Routes.ChooseGameCategory> { backstackEntry ->
-//            val gameCategory: Routes.ChooseGameCategory = backstackEntry.toRoute()
-//
-//            Scaffold(
-//                topBar = {
-//                    TriviaGoAppBar()
-//                }
-//            ) { innerPadding ->
-//                if () {
-//                }
-//            }
-//        }
-//    }
-//}
+@Composable
+fun TriviaGoApp() {
+    val navController = rememberNavController()
+    val viewModel: TriviaGoViewModel = viewModel()
+
+    NavHost(
+        navController = navController,
+        startDestination = Routes.Home
+    ) {
+        composable<Routes.Home> {
+            ChooseGameCategoryScreen(
+                onCategorySelected = { category ->
+                    viewModel.selectCategory(category)
+                    viewModel.startGame()
+                },
+                onProfileSettings = {
+                    navController.navigate(Routes.ProfileSettings)
+                }
+            )
+        }
+
+        composable<Routes.ProfileSettings> {
+            ProfileSettingsScreen(
+                viewModel = viewModel,
+                onBackHome = { navController.popBackStack() }
+            )
+        }
+
+    }
+}
