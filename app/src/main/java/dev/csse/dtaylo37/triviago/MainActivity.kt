4fa9020cfbe9504cf2a3975fc3f7039ec6b68d8e
Sanaia Pierre <122914227+sapierre26@users.noreply.github.com>
2026-03-06 13:35:08 -0800

    Trying to get everything from master branch

--- app/src/main/java/dev/csse/dtaylo37/triviago/MainActivity.kt
+++ app/src/main/java/dev/csse/dtaylo37/triviago/MainActivity.kt
@@ -4,15 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.lifecycle.viewmodel.compose.viewModel
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
 import dev.csse.dtaylo37.triviago.ui.*
 import dev.csse.dtaylo37.triviago.ui.theme.TriviaGOTheme
 
