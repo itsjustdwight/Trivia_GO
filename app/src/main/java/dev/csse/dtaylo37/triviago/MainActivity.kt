753366ec9566ea67b891c40274d3f0dfea976238
Sanaia Pierre <122914227+sapierre26@users.noreply.github.com>
2026-03-06 13:41:57 -0800

    Fully pulled from master branch; Updated colors to use the variables in the Theme.kt file

--- app/src/main/java/dev/csse/dtaylo37/triviago/MainActivity.kt
+++ app/src/main/java/dev/csse/dtaylo37/triviago/MainActivity.kt
@@ -4,7 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import dev.csse.dtaylo37.triviago.ui.*
+import dev.csse.dtaylo37.triviago.ui.TriviaGoApp
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import dev.csse.dtaylo37.triviago.ui.theme.TriviaGOTheme
 
 class MainActivity : ComponentActivity() {
