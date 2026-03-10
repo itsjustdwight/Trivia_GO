d085dfadfa898e6e4e2a98975d70e7d3f1817ef0
Sanaia Pierre <122914227+sapierre26@users.noreply.github.com>
2026-03-10 00:49:48 -0700

    Setup the RearrangeOrderQuestionScreen.kt file but need to fix some bugs; Next thing is to add functionality

---
+++ app/src/main/java/dev/csse/dtaylo37/triviago/ui/Icons.kt
@@ -0,0 +1,13 @@
+package dev.csse.dtaylo37.triviago.ui
+
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+
+class Icons(
+    imageVector: Any,
+    contentDescription: String,
+    tint: androidx.compose.ui.graphics.Color,
+    modifier: androidx.compose.ui.Modifier
+) {
+
+}
